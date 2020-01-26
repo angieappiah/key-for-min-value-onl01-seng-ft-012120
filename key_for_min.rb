3df67480466900942b98f75fcba286c501a75af4
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-26 04:52:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  value = "minimum"
+  Cars = { " red" => 4, "green" => 6, "pink" => 1 }
+  
 
 end
