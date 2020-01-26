715c1d18591f45f90fbdeadb90a5089ac861db92
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-26 05:01:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,12 @@
 def key_for_min_value(name_hash)
   value = "minimum"
   Cars = { " red" => 4, "green" => 6, "pink" => 1 }
-  
+  lowest_value = 0 
+  lowest_key = nil 
+  name_hash.each do |key,value|
+    if lowest_value == 0 
+      | | < lowest_value
+    end
+    
 
 end
